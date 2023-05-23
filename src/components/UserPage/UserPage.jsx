d3b3702aeb3c030ff63055801bb98f43e01de4b1
Luis Ortiz <l.ortizintec@gmail.com>
2023-05-23 16:47:53 -0400

    extracted information i wanted from business objects

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -6,11 +6,28 @@ import Search from '../Search/Search';
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const searchResults = useSelector((store) => store.results)
   return (
     <div className="container">
       <h2>Welcome, {user.username}, Lets Eat! </h2>
-      <LogOutButton className="btn" />
       <Search />
+      {
+        searchResults.map(result => {
+          return (
+            <div key={result.id}>
+              <h1>{result.name}</h1>
+              <img src={result.image_url} className='result-img' />
+              {!result.is_closed ? <p>Open Now</p> : <p>Closed</p>}
+              <p>{result.rating} out of 5 stars</p>
+              <p>{result.location.display_address}</p>
+              <p>{result.display_phone}</p>
+              <a href={result.url} target='_blank'>Visit Here</a>
+              <button className='btn'>Save</button>
+              <button className='btn'>Favorites</button>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
