c665286ef3e9caa14af29e01db6e84a0b7f013a1
Luis Ortiz <l.ortizintec@gmail.com>
2023-05-24 14:17:25 -0400

    able to display random suggestions on to the DOM

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Search from '../Search/Search';
 import ResultItem from '../ResultItem/ResultItem';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
-  const searchResults = useSelector((store) => store.results)
-  
+  const searchResults = useSelector((store) => store.results);
+  const suggestions = useSelector(store => store.suggestions);
+  const dispatch = useDispatch();
+
+  const reRoll = () => {
+    dispatch({ type: 'REROLL' })
+  }
+
   return (
     <div className="container">
       <h2>Welcome, {user.username}, Lets Eat! </h2>
+
+      {
+        suggestions.map((type, i) => {
+          return (
+            <p key={i}>{type}</p>
+          )
+        })
+      }
+      <button onClick={reRoll}>Re-Roll</button>
+
       <Search />
 
       {
@@ -20,7 +36,7 @@ function UserPage() {
           )
         })
       }
-      
+
     </div>
   );
 }
--- src/redux/reducers/suggestion.reducer.js
+++ src/redux/reducers/suggestion.reducer.js
@@ -1,14 +1,33 @@
-const foodTypes = ['Canadian', 'Caribbean', 'Cuban', 'Hawaiian', 'Mexican', 'Burmese', 'Cambodian', 'Chinese', 'Filipino', 
-'Indian', 'Indonesian', 'Japanese', 'Korean', 'Lebanese', 'Malaysian', 'Middle Eastern', 'Pakistani', 'Singaporean', 
-'Southeast Asian', 'Sri Lankan', 'Thai', 'Tibetan', 'Turkish', 'Vietnamese', 'Danish', 'French', 'German', 'Greek', 
-'Irish', 'Italian', 'Mediterranean', 'Norwegian', 'Polish', 'Portuguese', 'Romanian', 'Russian', 'Sicilian', 'Spanish', 
-'Swedish', 'Swiss', 'Ukrainian', 'Argentinean', 'Brazilian', 'Chilean', 'Latin American', 'Peruvian', 'South American', 
-'Venezuelan', 'Steak House', 'Pizza', 'Hot Dogs', 'Sandwich', 'Breakfast', 'Chicken', 'Ice Cream', 'Burgers', 'Pastries', 
-'Brunch', 'Bistro', 'Seafood', 'Vegan', 'Vegetarian', 'Grill', 'Jewish/Kosher', 'Soup', 'Lunch', 'Dinner', 'Organic', 
-'Noodles', 'African', 'Egyptian', 'Ethiopian', 'Brunch', 'Beef', 'Pork'];
+const foodTypes = ['Canadian', 'Caribbean', 'Cuban', 'Hawaiian', 'Mexican', 'Burmese', 'Cambodian', 'Chinese', 'Filipino',
+    'Indian', 'Indonesian', 'Japanese', 'Korean', 'Lebanese', 'Malaysian', 'Middle Eastern', 'Pakistani', 'Singaporean',
+    'Southeast Asian', 'Sri Lankan', 'Thai', 'Tibetan', 'Turkish', 'Vietnamese', 'Danish', 'French', 'German', 'Greek',
+    'Irish', 'Italian', 'Mediterranean', 'Norwegian', 'Polish', 'Portuguese', 'Romanian', 'Russian', 'Sicilian', 'Spanish',
+    'Swedish', 'Swiss', 'Ukrainian', 'Argentinean', 'Brazilian', 'Chilean', 'Latin American', 'Peruvian', 'South American',
+    'Venezuelan', 'Steak House', 'Pizza', 'Hot Dogs', 'Sandwich', 'Breakfast', 'Chicken', 'Ice Cream', 'Burgers', 'Pastries',
+    'Brunch', 'Bistro', 'Seafood', 'Vegan', 'Vegetarian', 'Grill', 'Jewish/Kosher', 'Soup', 'Lunch', 'Dinner', 'Organic',
+    'Noodles', 'African', 'Egyptian', 'Ethiopian', 'Brunch', 'Beef', 'Pork'];
 
-const suggestions = (state = foodTypes, action) => {
-    return state;
-  };
+const randomizer = () => {
+    return Math.floor(Math.random() * foodTypes.length);
+}
 
-  export default suggestions;
+const generateTypes = () => {
+    const suggest1 = foodTypes[randomizer()];
+    const suggest2 = foodTypes[randomizer()];
+    const suggest3 = foodTypes[randomizer()];
+    const suggest4 = foodTypes[randomizer()];
+    const suggest5 = foodTypes[randomizer()];
+    return [suggest1, suggest2, suggest3, suggest4, suggest5];
+}
+
+
+const suggestions = (state = generateTypes(), action) => {
+    switch (action.type) {
+        case 'REROLL':
+            return generateTypes();
+        default:
+            return state;
+    }
+};
+
+export default suggestions;
