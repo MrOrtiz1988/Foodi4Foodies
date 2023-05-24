92e8b65f85eaa29b6e6b7047e759de7d36be7b77
Luis Ortiz <l.ortizintec@gmail.com>
2023-05-24 15:38:22 -0400

    refactored suggestions in its own component and changed title in html

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,52 +1,18 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Search from '../Search/Search';
 import ResultItem from '../ResultItem/ResultItem';
-import { useState } from 'react';
+import Suggestions from '../../Suggestions/Suggestions';
 
 function UserPage() {
   const user = useSelector((store) => store.user);
   const searchResults = useSelector((store) => store.results);
-  const suggestions = useSelector(store => store.suggestions);
-  const [is_suggest, setIs_suggest] = useState(false);
-  const dispatch = useDispatch();
-
-  const reRoll = () => {
-    dispatch({ type: 'REROLL' });
-  }
-
-  const suggestToggle = () => {
-    if (!is_suggest) {
-      setIs_suggest(true);
-    } else {
-      setIs_suggest(false);
-    }
-  }
-
-  const chosen = (type) => {
-    dispatch({
-      type: 'SAGA/SEARCH',
-      payload: type
-  })
-  }
 
   return (
     <div className="container">
       <h2>Welcome {user.username}, Lets Eat! </h2>
 
-      <h4>Dont know what to eat? Click the suggestions button</h4>
-      {!is_suggest ?
-        <button onClick={suggestToggle}>Suggestions</button>
-        : <button onClick={suggestToggle}>Close</button>}
-
-      {is_suggest &&
-        suggestions.map((type, i) => {
-          return (
-            <p onClick={() => chosen(type)} key={i}>{type}</p>
-          )
-        })
-      }
-      {is_suggest && <button onClick={reRoll}>Re-Roll</button>}
+      <Suggestions />
 
       <Search />
 
