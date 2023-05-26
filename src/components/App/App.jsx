5a48d3500c0e27506ed5871848e48c923bf9f0dd
Luis Ortiz <l.ortizintec@gmail.com>
2023-05-26 13:01:04 -0400

    converted info page to saved list

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -15,7 +15,7 @@ import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
-import InfoPage from '../InfoPage/InfoPage';
+import SavedList from '../SavedList/SavedList';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
@@ -63,9 +63,9 @@ function App() {
           <ProtectedRoute
             // logged in shows InfoPage else shows LoginPage
             exact
-            path="/info"
+            path="/saved"
           >
-            <InfoPage />
+            <SavedList />
           </ProtectedRoute>
 
           <Route
--- src/components/SavedList/SavedList.jsx
+++ src/components/SavedList/SavedList.jsx
@@ -5,12 +5,12 @@ import React from 'react';
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is
 
-function InfoPage() {
+function SavedList() {
   return (
     <div className="container">
-      <p>Info Page</p>
+      <p>Saved Page</p>
     </div>
   );
 }
 
-export default InfoPage;
+export default SavedList;
