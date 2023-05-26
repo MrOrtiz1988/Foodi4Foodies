43faae1b0617f613a184035796136f2102cb2a09
Luis Ortiz <l.ortizintec@gmail.com>
2023-05-26 18:53:54 -0400

    savedList component base done

--- src/components/SavedList/SavedList.jsx
+++ src/components/SavedList/SavedList.jsx
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 function SavedList() {
 
   const dispatch = useDispatch();
+  const placeList = useSelector(store => store.placeList);
 
   useEffect(() => {
     dispatch({
@@ -15,6 +16,27 @@ function SavedList() {
   return (
     <div className="container">
       <h1>Saved Page</h1>
+
+      {
+        placeList.map(place => {
+          return (
+            <div className='result-div'>
+              <h2 className='result-title'>{place.name}</h2>
+              <img src={place.image} className='result-img' alt={place.name} />
+              <div className='result-details'>
+                <p>{place.rating} out of 5 stars</p>
+                <p>{place.address}</p>
+                <p>{place.phone}</p>
+                <a href={place.url} target='_blank'>Visit Here</a>
+              </div>
+              <br></br>
+              <div className='btn-div'>
+                <button className='btn'>Love it!</button>
+              </div>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
