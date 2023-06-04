a1646ddccb2f27e8f49182a52ea0c6d46c8d4ecc
Luis Ortiz <l.ortizintec@gmail.com>
2023-06-04 19:08:01 -0400

    started about page

---
+++ src/components/AboutPage/AboutPage.jsx
@@ -0,0 +1,8 @@
+function AboutPage () {
+    return (
+        <div>
+            <h3>Thoughts? Opinions? Let me know what you think! 😃</h3>
+            <h3>Coming Soon: </h3>
+        </div>
+    )
+}
